615834c067c9a284ad7505760eec27b5053eca6c
Luciagase <113980773+Luciagase@users.noreply.github.com>
2023-11-21 13:17:58 +0100

    csv

---
+++ ej2.py
@@ -0,0 +1,9 @@
+'''Limpieza y preprocesamiento de datos:
+• Identifiquen y traten o imputen los valores faltantes NaN usando una 
+función personalizada que use la moda/media para remplazarlos. En 
+caso de no haber filas con NaN, todo dato que esté por encima o por 
+debajo de dos desviaciones típicas de la media tendrá que imputarse 
+por la moda/media.
+• Conviertan los tipos de datos de las columnas si es necesario.
+• Renombren las columnas para facilitar su manipulación.'''
+
