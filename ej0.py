57a44b74ae2efdd6a74b2981a82518eff2059cc2
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-11-21 13:13:50 +0100

    cc

---
+++ ej0.py
@@ -0,0 +1,1 @@
+/usr/local/bin/python3 /Users/oliviaclemente/Documents/GitHub/Programaci-n_Trabajo/ej1.py
--- ej1.py
+++ ej1.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 
-
 archivo = 'R.csv'
 data = pd.read_csv(archivo)  # Utiliza pd.read_csv() para cargar los datos desde un archivo CSV
 
@@ -9,11 +8,11 @@ primeras = data.head()
 ultimas = data.tail()
 
 # Imprime las primeras y las últimas filas
-print(f"Primeras filas: {primeras}")
-print(f"Últimas 5 filas: {ultimas}")
+print(f"Primeras filas:\n{primeras}")
+print(f"Últimas 5 filas:\n{ultimas}")
 
-
-info = data.info()   # Muestra info del csv
+# Muestra info del csv
+data.info()
 
 nfilas, ncolumnas = data.shape
 print(f"Número de filas: {nfilas}")
