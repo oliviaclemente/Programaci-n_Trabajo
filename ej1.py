bac7a55c827e4e7b3c3fe9de946d5b0514b82628
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-11-21 13:01:50 +0100

    ccc

--- ej1.py
+++ ej1.py
@@ -11,3 +11,13 @@ ultimas = data.tail()
 # Imprime las primeras y las últimas filas
 print(f"Primeras filas: {primeras}")
 print(f"Últimas 5 filas: {ultimas}")
+
+
+info = data.info()   # Muestra info del csv
+
+nfilas, ncolumnas = data.shape
+print(f"Número de filas: {nfilas}")
+print(f"Número de columnas: {ncolumnas}")
+
+tipos = data.dtypes
+print(f"Tipos de datos de cada columna:{tipos}")
