ec41c5cacc486661017377fbc3ee6e68b235d3e5
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-12-05 11:51:30 +0100

    cc

--- ej4.2.py
+++ ej4.2.py
@@ -1,14 +1,25 @@
 import pandas as pd
 
+# Leer el archivo CSV
 archivo = 'Auto_Sales_data.csv'
 data = pd.read_csv(archivo)
 
-# Iterar sobre columnas
-for columna in data.columns:
-    # Acceder a los valores de cada fila en la columna actual
-    for valor in data[columna]:
-        # Realizar cálculos o cualquier operación necesaria
-        resultado = valor * 2
+# Ejemplo 1: Crear una nueva columna que sea la suma de dos columnas existentes
+data['Nueva_Columna'] = data['Columna1'] + data['Columna2']
 
-        # Puedes imprimir o hacer lo que desees con el resultado
-        print(resultado)
+# Ejemplo 2: Crear una nueva columna que sea el producto de dos columnas existentes
+data['Otra_Nueva_Columna'] = data['Columna3'] * data['Columna4']
+
+# Ejemplo 3: Aplicar una función a una columna existente para crear una nueva columna
+def calcular_nueva_columna(valor):
+    return valor * 2
+
+data['Columna_Nueva'] = data['Columna_Existente'].apply(calcular_nueva_columna)
+
+# Ejemplo 4: Crear una nueva columna basada en una condición
+data['Nueva_Columna_Condicional'] = np.where(data['Columna_Condicional'] > 10, 'Mayor', 'Menor')
+
+# Puedes seguir combinando funciones y operaciones según tus necesidades.
+
+# Guardar el DataFrame modificado en un nuevo archivo CSV
+data.to_csv('Nuevo_Archivo.csv', index=False)
