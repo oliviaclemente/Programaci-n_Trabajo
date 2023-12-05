6494a85bc18ce267ff12eece0de911fe7b3a1329
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-12-05 12:07:42 +0100

    cc

--- ej4.1.py
+++ ej4.1.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-archivo = 'Auto_Sales_data.csv'
+archivo = 'Nuevo_archivo.csv'
 data = pd.read_csv(archivo)
 
 #Filtramos las filas donde QUANTITYORDERED >50
@@ -10,18 +10,3 @@ condicion_filtrado = data['QUANTITYORDERED'] > 50
 datos_filtrados = data.loc[condicion_filtrado]
 print(datos_filtrados)
 
-
-# Verifica los nombres de las columnas en tu DataFrame
-print(data.columns)
-
-# Agreguemos una nueva columna llamada 'Estado' basada en las ventas anuales
-for indice, fila in data.iterrows():
-    if 'Ventas_Anuales' in data.columns and fila['Ventas_Anuales'] > 10000:
-        data.at[indice, 'Estado'] = 'Alto'
-    elif 'Ventas_Anuales' in data.columns and 5000 <= fila['Ventas_Anuales'] <= 10000:
-        data.at[indice, 'Estado'] = 'Moderado'
-    else:
-        data.at[indice, 'Estado'] = 'Bajo'
-
-# Imprime el DataFrame modificado
-print(data)
