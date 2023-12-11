3d25bc4febb5471395481bf64687748472e28b06
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2023-12-11 10:54:31 +0100

    cc

--- ej1.py
+++ ej1.py
@@ -4,6 +4,8 @@ import pandas as pd
 archivo = 'Auto_Sales_data.csv'
 data = pd.read_csv(archivo)  # Utiliza pd.read_csv() para cargar los datos desde un archivo CSV
 
+
+#Muestren las primeras y últimas filas del dataset.
 primeras = data.head()
 ultimas = data.tail()
 
@@ -14,10 +16,11 @@ print(f"Últimas 5 filas:\n{ultimas}")
 # Muestra info del csv
 data.info()
 
-nfilas, ncolumnas = data.shape
+#Obtengan información general del dataset, como el número de filas y columnas, y los tipos de datos.
+nfilas, ncolumnas = data.shape  #la funcion.shape devuelve una tupla que representa las dimensiones de la estructura de datos
 print(f"Número de filas: {nfilas}")
 print(f"Número de columnas: {ncolumnas}")
 
-tipos = data.dtypes
-print(f"Tipos de datos de cada columna:{tipos}")
+tipos = data.dtypes  #un atributo de un DataFrame que devuelve una Serie que contiene los tipos de datos de cada columna en el DataFrame
+print(f"Tipos de datos de cada columna:{tipos}")  
 
